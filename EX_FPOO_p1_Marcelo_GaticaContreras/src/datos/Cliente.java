af695b9bc7a81ee3974cc607a5f6aa764a6ad9b8
XZeromarx <marcelometal15@live.com>
2017-11-29 08:25:13 -0200

    Se agrega doc. de texto que da info de las clases creadas, falta hacer metodos de impresion y Javadoc.

--- EX_FPOO_p1_Marcelo_GaticaContreras/src/datos/Cliente.java
+++ EX_FPOO_p1_Marcelo_GaticaContreras/src/datos/Cliente.java
@@ -1,5 +1,7 @@
 package datos;
 
+import java.util.Objects;
+
 /**
  *
  * @author XZeroMarx
@@ -20,6 +22,54 @@ public class Cliente {
     public Cliente(String rut, String nombre) {
         this.rut = rut;
         this.nombre = nombre;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "rut=" + rut + ", nombre=" + nombre + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        if (!Objects.equals(this.rut, other.rut)) {
+            return false;
+        }
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        return true;
     }
     
     
--- EX_FPOO_p1_Marcelo_GaticaContreras/src/datos/Pasaje.java
+++ EX_FPOO_p1_Marcelo_GaticaContreras/src/datos/Pasaje.java
@@ -1,10 +1,11 @@
 package datos;
+
 /**
  *
  * @author XZeroMarx
  */
-public class Pasaje extends Cliente{
-    
+public class Pasaje extends Cliente {
+
     private int numeroVuelo;
     private int fechaVuelo;
     private int horaVuelo;
@@ -12,7 +13,127 @@ public class Pasaje extends Cliente{
     private int numeroAsiento;
     private int clase;
     private int valordeVuelo;
-    
-    
-     
+
+    public Pasaje() {
+    }
+
+    public Pasaje(int numeroVuelo, int fechaVuelo, int horaVuelo, int destinoVuelo, int numeroAsiento, int clase, int valordeVuelo) {
+        this.numeroVuelo = numeroVuelo;
+        this.fechaVuelo = fechaVuelo;
+        this.horaVuelo = horaVuelo;
+        this.destinoVuelo = destinoVuelo;
+        this.numeroAsiento = numeroAsiento;
+        this.clase = clase;
+        this.valordeVuelo = valordeVuelo;
+    }
+
+    public Pasaje(int numeroVuelo, int numeroAsiento, int valordeVuelo) {
+        this.numeroVuelo = numeroVuelo;
+        this.numeroAsiento = numeroAsiento;
+        this.valordeVuelo = valordeVuelo;
+    }
+
+    public int getNumeroVuelo() {
+        return numeroVuelo;
+    }
+
+    public void setNumeroVuelo(int numeroVuelo) {
+        this.numeroVuelo = numeroVuelo;
+    }
+
+    public int getFechaVuelo() {
+        return fechaVuelo;
+    }
+
+    public void setFechaVuelo(int fechaVuelo) {
+        this.fechaVuelo = fechaVuelo;
+    }
+
+    public int getHoraVuelo() {
+        return horaVuelo;
+    }
+
+    public void setHoraVuelo(int horaVuelo) {
+        this.horaVuelo = horaVuelo;
+    }
+
+    public int getDestinoVuelo() {
+        return destinoVuelo;
+    }
+
+    public void setDestinoVuelo(int destinoVuelo) {
+        this.destinoVuelo = destinoVuelo;
+    }
+
+    public int getNumeroAsiento() {
+        return numeroAsiento;
+    }
+
+    public void setNumeroAsiento(int numeroAsiento) {
+        this.numeroAsiento = numeroAsiento;
+    }
+
+    public int getClase() {
+        return clase;
+    }
+
+    public void setClase(int clase) {
+        this.clase = clase;
+    }
+
+    public int getValordeVuelo() {
+        return valordeVuelo;
+    }
+
+    public void setValordeVuelo(int valordeVuelo) {
+        this.valordeVuelo = valordeVuelo;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Pasaje other = (Pasaje) obj;
+        if (this.numeroVuelo != other.numeroVuelo) {
+            return false;
+        }
+        if (this.fechaVuelo != other.fechaVuelo) {
+            return false;
+        }
+        if (this.horaVuelo != other.horaVuelo) {
+            return false;
+        }
+        if (this.destinoVuelo != other.destinoVuelo) {
+            return false;
+        }
+        if (this.numeroAsiento != other.numeroAsiento) {
+            return false;
+        }
+        if (this.clase != other.clase) {
+            return false;
+        }
+        if (this.valordeVuelo != other.valordeVuelo) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Pasaje{" + "numeroVuelo=" + numeroVuelo + ", fechaVuelo=" + fechaVuelo + ", horaVuelo=" + horaVuelo + ", destinoVuelo=" + destinoVuelo + ", numeroAsiento=" + numeroAsiento + ", clase=" + clase + ", valordeVuelo=" + valordeVuelo + '}';
+    }
+
 }
